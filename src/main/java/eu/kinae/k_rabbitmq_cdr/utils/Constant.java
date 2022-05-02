06aab4cf58854dc74821482704d3f527b5f629b2
Anthony Defraine <anthony.defraine@ekwateur.fr>
2022-05-02 08:21:04 +0200

    Remove the character n in message name

--- src/main/java/eu/kinae/k_rabbitmq_cdr/utils/Constant.java
+++ src/main/java/eu/kinae/k_rabbitmq_cdr/utils/Constant.java
@@ -3,7 +3,7 @@ package eu.kinae.k_rabbitmq_cdr.utils;
 public final class Constant {
 
     public static final String PROJECT_NAME = "k-rabbitmq-cdr";
-    public static final String FILE_PREFIX = PROJECT_NAME + "_n";
+    public static final String FILE_PREFIX = PROJECT_NAME + "_";
     public static final String FILE_PROPERTIES_SUFFIX = "_props.json";
 
     private Constant() {
