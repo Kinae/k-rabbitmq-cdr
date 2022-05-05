da634cee071e142b2d829170b93895b189733d8f
Anthony Defraine <anthony.defraine@ekwateur.fr>
2022-05-05 23:14:18 +0200

    Remove unused code

--- src/main/java/eu/kinae/k_rabbitmq_cdr/utils/KMessage.java
+++ src/main/java/eu/kinae/k_rabbitmq_cdr/utils/KMessage.java
@@ -3,7 +3,6 @@ package eu.kinae.k_rabbitmq_cdr.utils;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.StringJoiner;
 
 import com.rabbitmq.client.AMQP;
 
@@ -11,10 +10,6 @@ public record KMessage(AMQP.BasicProperties properties, byte[] body, long messag
 
     public KMessage(AMQP.BasicProperties properties, byte[] body) {
         this(properties, body, 0, 0);
-    }
-
-    public KMessage(AMQP.BasicProperties properties, String body) {
-        this(properties, body.getBytes(), 0, 0);
     }
 
     public KMessage(AMQP.BasicProperties properties, byte[] body, long deliveryTag) {
@@ -40,16 +35,6 @@ public record KMessage(AMQP.BasicProperties properties, byte[] body, long messag
         int result = Objects.hash(properties);
         result = 31 * result + Arrays.hashCode(body);
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return new StringJoiner(" | ", KMessage.class.getSimpleName() + "[", "]")
-                .add("properties=" + properties)
-                .add("body=" + new String(body))
-                .add("messageCount=" + messageCount)
-                .add("deliveryTag=" + deliveryTag)
-                .toString();
     }
 
 }
